cb68a3aba541e353466b9e5c0dbbdfd2e7413da4
unknown <vanoppen.christof@hotmail.com>
2014-08-02 13:42:47 +0200

    updated app.js - updates

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ mongoose = require('mongoose');
 
 server.listen(3000);
 
-app.get('/scoreboard.html', function(req, res){
+
+app.get('/', function(req, res){
 	res.sendfile(__dirname + '/scoreboard.html');
 });
 app.get('/admin.html', function(req, res){
 	res.sendfile(__dirname + '/admin.html');
+});
+app.use(express.static(__dirname + '/public'));
+
+io.sockets.on('connection', function(socket){
+	socket.on('send message', function(data){
+		io.sockets.emit('new message', data);
+	});
 });
